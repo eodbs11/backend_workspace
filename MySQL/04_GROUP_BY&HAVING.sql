e9cd31f56a2b3df2b26838800fd53b6a7cb5a58e
eodbs11 <152463186+eodbs11@users.noreply.github.com>
2023-12-27 17:51:14 +0900

    1227 01

--- MySQL/04_GROUP_BY&HAVING.sql
+++ MySQL/04_GROUP_BY&HAVING.sql
@@ -6,35 +6,40 @@
 
 -- 각 부서별 조회
 SELECT dept_code, 
-count(*) "사원 수",
-sum(salary) "총 급여",
-avg(coalesce(salary, 0)) "평균 급여",
-min(salary) "최저 급여",
-max(salary) "최고 급여"
+	count(*) "사원 수",
+	sum(salary) "총 급여",
+	avg(coalesce(salary, 0)) "평균 급여",
+	min(salary) "최저 급여",
+	max(salary) "최고 급여"
 from employee
 GROUP BY dept_code
 order by dept_code; -- 1
 
--- 직급 코드
+-- 직급 코드(job_code)별 사원 수 조회
+select job_code, count(*)
+from employee
+group by job_code;
 
 -- 성별 별(남자/여자) 사원 수 조회
-
 select	
 	if(substr(emp_no, 8, 1) = 1, '남자', '여자') "성별",
     count(*) "사원 수"
-	from employee
-    group by 성별;
+from employee
+group by 성별;
 
 /*
 	HAVING
     - 그룹에 대한 조건을 제시할 때 사용하는 구문
     
-    select * | 조회하고자 하는 컬럼명 별칭|함수
-    
-    order by 컬럼명 
+    5 select * | 조회하고자 하는 컬럼명 별칭|함수
+    1 FROM 조회하고자 하는 테이블명
+    2 WHERE 조건식 (연산자들 가지고 기술)
+    3 GROUP BY 그룹 기준에 해당하는 컬럼명|함수
+    4 HAVING 조건식 (그룹 함수를 가지고 기술)
+    6 ORDER BY 컬러명 | 컬럼순번 | 별칭 [ASC|DESC];    
 */
 
--- 부서별 평균 급여 조회
+-- 부서별 평균 급여가 300만원 이상인 부서의 평균 급여 조회
 select 
 	dept_code, 
     format(avg(ifnull(salary, 0)), 0) "평균 급여"
@@ -102,7 +107,7 @@ group by dept_code, job_code with rollup;
 -- EMPLOYEE 테이블에서 부서 코드가 D5인 사원 또는 급여가 300만원 초과인
 -- 사원들의 사변, 사원명, 부서코드, 급여 조회
 
--- (1) 부서코드가 DS인 사원alter
+-- (1) 부서코드가 DS인 사원들
 select emp_id, emp_name, dept_code, salary
 from employee
 where dept_code = 'D5'
